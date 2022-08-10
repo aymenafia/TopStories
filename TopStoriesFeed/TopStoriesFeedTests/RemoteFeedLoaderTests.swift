0b47aaf5d2acba20b938a262b34395af59d3ebab
Aymen Aifa <mohamedaymen.afia@esprit.tn>
2022-08-10 23:46:23 +0100

    create RemoteFeedLoader test file

--- TopStoriesFeed/TopStoriesFeedTests/RemoteFeedLoaderTests.swift
+++ TopStoriesFeed/TopStoriesFeedTests/RemoteFeedLoaderTests.swift
@@ -6,30 +6,67 @@
 //
 
 import XCTest
+import TopStoriesFeed
+
 
 class RemoteFeedLoaderTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    
+    func test_init_doesNotRequestDataFromURL() {
+        let url = URL(string: "https://a-GIVEN-url.com")!
+        let (_, client) = makeSUT(url: url)
+        XCTAssertTrue(client.requestedURLs.isEmpty)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func test_load_requestDataFromURL() {
+        //Arrange: given a client and sut
+        let url = URL(string: "https://a-GIVEN-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        
+        sut.load()
+        XCTAssertEqual(client.requestedURLs, [url])
     }
+    
+    
+    func test_load_deliversErrorOnClientError() {
+        
+        let (sut, client) = makeSUT()
+        client.error = NSError(domain: "Test", code: 0)
+        var capturedError: RemoteFeedLoader.Error?
+        sut.load { error in capturedError = error }
+        XCTAssertEqual(capturedError, .connectivity)
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func test_loadTwice_requestDataFromURLTwice() {
+        //Arrange: given a client and sut
+        let url = URL(string: "https://a-GIVEN-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        
+        sut.load()
+        sut.load()
+        XCTAssertEqual(client.requestedURLs, [url, url])
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(url: URL = URL(string: "https://a-GIVEN-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(url: url, client: client)
+        return (sut, client)
+    }
+    
+    class HTTPClientSpy: HTTPClient {
+        
+        var requestedURLs = [URL]()
+        var error: Error?
+        
+        func get(from url: URL, completion: @escaping (Error) -> Void) {
+            if let error = error {
+                completion(error)
+            }
+            requestedURLs.append(url)
         }
     }
-
 }
+
