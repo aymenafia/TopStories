c14c81542b73c3809f4e1d8547a65608041fe788
Aymen Aifa <mohamedaymen.afia@esprit.tn>
2022-08-15 15:36:51 +0100

    Move `FeedViewController` to production target

---
+++ TopStoriesFeed/TopStoriesFeediOS/FeedViewController.swift
@@ -0,0 +1,33 @@
+//
+//  FeedViewController.swift
+//  TopStoriesFeediOS
+//
+//  Created by Aymen Aifa on 15.08.22.
+//
+
+import TopStoriesFeed
+import UIKit
+
+final public class FeedViewController: UITableViewController {
+    private var loader: FeedLoader?
+    
+    public convenience init(loader: FeedLoader) {
+        self.init()
+        self.loader = loader
+    }
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
+        load()
+    }
+    
+    @objc private func load() {
+        refreshControl?.beginRefreshing()
+        loader?.load { [weak self] _ in
+            self?.refreshControl?.endRefreshing()
+        }
+    }
+}
