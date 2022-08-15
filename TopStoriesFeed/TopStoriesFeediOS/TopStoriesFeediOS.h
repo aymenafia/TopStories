9101308fe26868ff0edc3b8d348dcff59cb7399f
Aymen Aifa <mohamedaymen.afia@esprit.tn>
2022-08-15 14:05:40 +0100

    Add `TopStoriesFeediOS` framework (prod and test) targets for iOS platform-specific components

---
+++ TopStoriesFeed/TopStoriesFeediOS/TopStoriesFeediOS.h
@@ -0,0 +1,18 @@
+//
+//  TopStoriesFeediOS.h
+//  TopStoriesFeediOS
+//
+//  Created by Aymen Aifa on 15.08.22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TopStoriesFeediOS.
+FOUNDATION_EXPORT double TopStoriesFeediOSVersionNumber;
+
+//! Project version string for TopStoriesFeediOS.
+FOUNDATION_EXPORT const unsigned char TopStoriesFeediOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TopStoriesFeediOS/PublicHeader.h>
+
+
